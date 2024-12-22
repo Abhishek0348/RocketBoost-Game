48993d1467fdfe68ee11e5ca5e376506772f9b7f
Abhishek Sharma <141435158+SurabhiTupe02@users.noreply.github.com>
2024-12-22 17:16:46 +0530

    more of implementations done like physics, audio

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,17 +4,63 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustForce = 1000f;
+    [SerializeField] float rotationForce = 100f;
+    AudioSource audioSource;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Dang,I need some space!");
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float RotationValue = rotation.ReadValue<float>();
+        if(RotationValue < 0)
+        {
+            ApplyRotation(rotationForce);
+        }
+        else if (RotationValue > 0)
+        {
+            ApplyRotation(-rotationForce);
+        }
+    }
+
+    private void ApplyRotation(float RotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * RotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
